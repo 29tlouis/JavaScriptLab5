5aaacdf6ff64bffc7921d5cccdf73f64754a636a
29tlouis <29tlouis@cpsd.us>
2026-06-10 12:18:21 -0400

    Made a little change to get the screen shots for my lab

--- js/03_const.js
+++ js/03_const.js
@@ -4,7 +4,7 @@ const z = 6;
 // 2.  use a console.log statement to log the value of z to the console.  Paste a snip of the page and the console log in your lab document.
 console.log(z);
 // 3.  redeclare z using const and assign it the value 3.  Refresh your browser and note what happens.  Copy a picture of your page and the console log into your lab document. 
-const z = 3;
+
 // 4.  Delete the "const" you typed in the previous line, refresh your page.  Copy a picture of your page and console log into your lab document.
 
 // 5. Delete the z = 3 above and refresh your page.  What happens when you try to change the value of a variable that has been declared with "const"?  Answer in your lab document
